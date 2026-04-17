60aa81fb8d5b8bbaefef21418abea98ad9e1d1cd
Augusto <augustocarmonaperez@gmail.com>
2026-04-17 19:16:02 -0300

    feat: se reestructuró el proyecto y se agregó un ejemplo de operador ternario

---
+++ assets/scripts/operador_ternario.js
@@ -0,0 +1,9 @@
+let nombre = prompt("Ingresá tu nombre:") 
+
+// if (nombre) {
+//     alert("Hola " + nombre)
+// } else {
+//     alert("No ingresaste tu nombre")
+// }
+
+nombre ? alert("Hola " + nombre) : alert("No ingresaste tu nombre")
